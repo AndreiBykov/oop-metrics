d7f1b3adf24e1e2282714b28feabead8d350e71b
Andrey Bykov <dronbykov@gmail.com>
2018-04-17 01:01:34 +0300

    started creating getInvokedMethods()

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -35,23 +35,23 @@ public class App {
                 JComponent.class, Frame.class, Dialog.class, JWindow.class,
                 Applet.class, JFrame.class, JDialog.class, JApplet.class);
 
-        for (Class aClass : classes) {
-            List<CtMethod> publicMethods = getPublicMethods(aClass.getName());
-            Map<CtMethod, Set<String>> methodsWithFields = publicMethods.stream()
-                .collect(Collectors.toMap(method -> method, App::getUsedFields));
-            int count = 0;
-            for (int i = 0; i < publicMethods.size(); i++) {
-                for (int j = i + 1; j < publicMethods.size(); j++) {
-                    Set<String> fields1 = new HashSet<>(methodsWithFields.get(publicMethods.get(i)));
-                    Set<String> fields2 = new HashSet<>(methodsWithFields.get(publicMethods.get(j)));
-                    fields1.retainAll(fields2);
-                    if (fields1.size() > 0) {
-                        count++;
-                    }
-                }
-            }
-            System.out.println(aClass.getName() + "\t" + publicMethods.size() + "\t" + count);
-        }
+//        for (Class aClass : classes) {
+//            List<CtMethod> publicMethods = getPublicMethods(aClass.getName());
+//            Map<CtMethod, Set<String>> methodsWithFields = publicMethods.stream()
+//                .collect(Collectors.toMap(method -> method, App::getUsedFields));
+//            int count = 0;
+//            for (int i = 0; i < publicMethods.size(); i++) {
+//                for (int j = i + 1; j < publicMethods.size(); j++) {
+//                    Set<String> fields1 = new HashSet<>(methodsWithFields.get(publicMethods.get(i)));
+//                    Set<String> fields2 = new HashSet<>(methodsWithFields.get(publicMethods.get(j)));
+//                    fields1.retainAll(fields2);
+//                    if (fields1.size() > 0) {
+//                        count++;
+//                    }
+//                }
+//            }
+//            System.out.println(aClass.getName() + "\t" + publicMethods.size() + "\t" + count);
+//        }
 
 //        List<CtMethod> publicMethods = getPublicMethods("java.awt.Panel");
 //        System.out.println(publicMethods.size());
@@ -60,14 +60,13 @@ public class App {
 //            .collect(Collectors.toMap(method -> method, App::getUsedFields));
 //
 //        methodsWithFields.forEach((k, v) -> System.out.println(k.getName() + "\t: " + v));
+
+        List<CtMethod> publicMethods = getPublicMethods("java.awt.Dialog");
+        publicMethods.forEach(App::getInvokedMethods);
     }
 
     private static Set<String> getUsedFields(CtMethod method) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(baos);
-        InstructionPrinter i = new InstructionPrinter(ps);
-        i.print(method);
-        String content = baos.toString();
+        String content = getMethodContent(method);
 
         return Arrays.stream(content.split("\\r?\\n"))
             .filter(string -> string.contains("field"))
@@ -80,7 +79,7 @@ public class App {
         ClassPool pool = ClassPool.getDefault();
         try {
             CtClass ctClass = pool.get(className);
-            CtMethod[] ctMethods = ctClass.getDeclaredMethods();
+            CtMethod[] ctMethods = ctClass.getDeclaredMethods(); // should be getMethods() since inherited methods are also needed
             return Arrays.stream(ctMethods)
                 .filter(ctMethod -> Modifier.isPublic(ctMethod.getModifiers()))
                 .collect(Collectors.toList());
@@ -88,5 +87,30 @@ public class App {
             e.printStackTrace();
             throw new RuntimeException();
         }
+    }
+
+    private static void getInvokedMethods(CtMethod method) {
+        String content = getMethodContent(method);
+
+        System.out.println(method.getName());
+        System.out.println();
+
+        Arrays.stream(content.split("\\r?\\n"))
+            .filter(string -> string.contains("invoke"))
+            .filter(string -> !string.contains("<init>"))
+            .forEach(System.out::println);
+//            .map(string -> string.replaceAll(".*Field ", ""))
+//            .map(string -> string.replaceAll("\\(.+\\)", ""))
+//            .collect(Collectors.toCollection(LinkedHashSet::new));
+        System.out.println("-----------------------");
+
+    }
+
+    private static String getMethodContent(CtMethod method) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        InstructionPrinter i = new InstructionPrinter(ps);
+        i.print(method);
+        return baos.toString();
     }
 }
